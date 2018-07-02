c5915751c2d98b4434908bc7e1b565007ab0a69c
xupeiyuan <xupeiyuan@zhuanzhuan.com>
2018-07-02 14:51:16 +0800

    add plugin config

--- vue.config.js
+++ vue.config.js
@@ -13,5 +13,12 @@ module.exports = {
     // and falls back to `public/index.html` if not found.
     // Output filename is inferred to be `subpage.html`.
     subpage: 'src/subpage/main.js'
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        return args
+      })
   }
 }
